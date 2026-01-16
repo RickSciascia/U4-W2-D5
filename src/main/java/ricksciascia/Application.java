92efb6f4556fd78584ecb55a2bb3dd65e597aeab
Riccardo <richi-97@live.it>
2026-01-16 16:05:00 +0100

    metodi aggiungi rimuovi e stampa

--- src/main/java/ricksciascia/Application.java
+++ src/main/java/ricksciascia/Application.java
@@ -7,19 +7,32 @@ import ricksciascia.entities.Videogioco;
 public class Application {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Collezione collezione = new Collezione();
 
-        collezione.addNew(new Videogioco("V1","Cyberpunk 2077",2020,39.90,"PC", 120, Genere.RPG));
+        collezione.addNew(new Videogioco("V1","Cyberpunk",2020,39.90,"PC", 120, Genere.RPG));
         collezione.addNew(new Videogioco("V2","The Witcher 3",2015,19.90,"PC", 100, Genere.RPG));
         collezione.addNew(new GiocoDaTavolo("T1","Monopoli",1970,29.90,2,240));
         collezione.addNew(new GiocoDaTavolo("T2","Risiko",2000,59.90,4,300));
-//        collezione.addNew(new Videogioco("V1","test",1999,10.00,"PC",10,Genere.RPG));
+        collezione.addNew(new Videogioco("V3","Project Cars",1999,100.00,"PC",30,Genere.SIM));
+//        Videogioco newVideogioco = new Videogioco("V1","Cyberpunk 2077",2020,49.90,"PC",120,Genere.RPG);
+        int scelta = -1;
 
 //        System.out.println(collezione.searchById("T2"));
 //        System.out.println(collezione.searchById("T3"));
 //        System.out.println(collezione.searchByPrice(19.99));
 //        System.out.println(collezione);
 //        System.out.println(collezione.searchByPlayerNumber(2));
-        
+//        collezione.removeByID("V1");
+//        collezione.updateById("V1",newVideogioco);
+//        System.out.println(collezione);
+//        collezione.printStatsCollection();
+        do {
+            System.out.println("---- Interfaccia collezione giochi ----");
+            System.out.println("1. Aggiungi Gioco");
+            System.out.println("2. Ricerca Gioco tramite ID");
+            System.out.println("3. Ricerca Giochi tramite prezzo");
+        }
+        while(scelta!=0);
     }
 }
--- src/main/java/ricksciascia/Collezione.java
+++ src/main/java/ricksciascia/Collezione.java
@@ -2,10 +2,9 @@ package ricksciascia;
 
 import ricksciascia.entities.Gioco;
 import ricksciascia.entities.GiocoDaTavolo;
+import ricksciascia.entities.Videogioco;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Collezione {
     private Map<String, Gioco> collezione = new HashMap<>();
@@ -37,6 +36,64 @@ public class Collezione {
                 .map(gioco -> (GiocoDaTavolo) gioco)
                 .filter(gioco -> gioco.getNumberOfPlayers() == nGiocatori).toList();
         return ricercaPerGiocatori;
+    }
+    public void removeByID(String id) throws IllegalArgumentException {
+        if(collezione.containsKey(id)) {
+            collezione.remove(id);
+            System.out.println("Elemento rimosso correttamente");
+        } else {
+            throw new IllegalArgumentException("Errore: L'Id inserito non è presente");
+        }
+    }
+
+    public void updateById(String id, Gioco giocoAggiornato) throws IllegalArgumentException {
+        if(collezione.containsKey(id)) {
+            if(giocoAggiornato.getId().equals(id)) {
+                if(giocoAggiornato.getClass().equals(collezione.get(id).getClass())) {
+                    collezione.put(id, giocoAggiornato);
+                    System.out.println("Il gioco con ID " + id + " è stato sovrascritto");
+                }
+                else{
+                    throw new IllegalArgumentException("Errore : Stai provando ad aggiornare un gioco di categoria Videogioco con un gioco di categoria Gioco Da Tavolo o viceversa");
+                }
+            }
+            else {
+                throw new IllegalArgumentException("Errore: L'id del gioco che vuoi aggiornare " + id + " non corrisponde all id che fornisci nella sua versione aggiornata! " + giocoAggiornato.getId());
+            }
+
+        }
+        else {
+                throw new IllegalArgumentException("ID non trovato");
+        }
+    }
+
+    public void printStatsCollection() {
+        if(collezione.isEmpty()) {
+            System.out.println("Non ci sono elementi nella collezione non posso calcolare le statistiche");
+            return;
+        }
+        int nVG = collezione.values().stream()
+                .filter(gioco-> gioco instanceof Videogioco).toList().size();
+
+        int nGDT = collezione.values().stream()
+                .filter(gioco -> gioco instanceof GiocoDaTavolo).toList().size();
+        List<Gioco> giocoCaro = collezione.values().stream()
+                .sorted(Comparator.comparingDouble(Gioco::getPrezzo).reversed()).limit(1).toList();
+
+        OptionalDouble avgPrice = collezione.values().stream()
+                .mapToDouble(gioco -> gioco.getPrezzo()).average();
+
+        System.out.println("---------- STATISTICHE DELLA COLLEZIONE ----------");
+        System.out.println("Numero di Videogiochi: " + nVG);
+        System.out.println("Numero di Giochi da Tavolo: " + nGDT);
+        if(avgPrice.isPresent()) {
+            System.out.println("La media prezzi è: " + avgPrice.getAsDouble());
+        }
+        if(!giocoCaro.isEmpty()) {
+            System.out.println("Il gioco più costoso è : " + giocoCaro.getFirst().getTitolo() + " " +  giocoCaro.getFirst().getPrezzo() + " euro");
+        }
+        System.out.println("---------- FINE STATISTICHE DELLA COLLEZIONE ----------");
+
     }
 
     @Override
